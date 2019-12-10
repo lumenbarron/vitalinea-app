a7ba08df89b948a5667f15a340b1a69fed43a772
lumenbarron <lucemendez@gmail.com>
2019-12-10 16:14:09 -0600

    Updates

---
+++ precache-manifest.0848c483c24f8152bba226006d38b2f7.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff9373fc92a0821f4f247ecaad124ac8",
+    "url": "/vitalinea-app/index.html"
+  },
+  {
+    "revision": "aa8aaf70f296e04da17e",
+    "url": "/vitalinea-app/static/css/main.25e47495.chunk.css"
+  },
+  {
+    "revision": "e11bc790ab03abd0b33f",
+    "url": "/vitalinea-app/static/js/2.026d49e4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/vitalinea-app/static/js/2.026d49e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa8aaf70f296e04da17e",
+    "url": "/vitalinea-app/static/js/main.49294b4a.chunk.js"
+  },
+  {
+    "revision": "01d399f36519847efd06",
+    "url": "/vitalinea-app/static/js/runtime-main.93a23385.js"
+  },
+  {
+    "revision": "27cd4ab3d59722433782984c55ab36a7",
+    "url": "/vitalinea-app/static/media/background-mobile-a.27cd4ab3.png"
+  },
+  {
+    "revision": "0e2e75afda436fb2cea5c520f7becfe9",
+    "url": "/vitalinea-app/static/media/background-web.0e2e75af.png"
+  },
+  {
+    "revision": "487a2166c135497ed2eee20a3cc8e6eb",
+    "url": "/vitalinea-app/static/media/bg-familia-vitalinea.487a2166.png"
+  },
+  {
+    "revision": "4d88149169ccc6771b332f7aec671d4d",
+    "url": "/vitalinea-app/static/media/img-bote-fresa.4d881491.png"
+  },
+  {
+    "revision": "8fb9871121f9808ec0fb9e25c8d12bf3",
+    "url": "/vitalinea-app/static/media/img-vitalinea-bebible.8fb98711.png"
+  },
+  {
+    "revision": "ee25613388991988c4d6f26f658f4ae9",
+    "url": "/vitalinea-app/static/media/img-vitalinea-griego.ee256133.png"
+  },
+  {
+    "revision": "aaf84db02e7e2a7c9b3074e94f88d9b4",
+    "url": "/vitalinea-app/static/media/img-vitalinea-sin-azucar.aaf84db0.png"
+  }
+]);
